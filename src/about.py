f54851d773e6636c320fbbc32b170ca92410df20
Ayandeep Roy <ayandeeproy460@gmail.com>
2026-04-07 22:56:49 +0530

    Update about.py

--- src/about.py
+++ src/about.py
@@ -12,7 +12,7 @@ def show_about_page():
     col1, col2, col3 = st.columns([1,0.2,2])
 
     with col1:
-        st.image("assets\mentor.png", width=180)
+        st.image("assets/mentor.png", width=180)
 
     with col2:
         print("")
@@ -31,21 +31,21 @@ def show_about_page():
     col1, col2 = st.columns([1,1])
 
     with col1:
-        st.image("assets\\arnav.png", width=150)
+        st.image("assets/arnav.png", width=150)
         st.markdown("**Arnav Biswas**  \nCSE Student")
 
     with col2:
-        st.image("assets\\ayan.jpeg", width=100)
+        st.image("assets/ayan.jpeg", width=100)
         st.markdown("**Ayandeep Roy**  \nCSE Student")
     
     col3, col4 = st.columns([1,1])
 
     with col3:
-        st.image("assets\\rimi.png", width=120)
+        st.image("assets/rimi.png", width=120)
         st.markdown("**Rimi Dutta**  \nCSE Student")
 
     with col4:
-        st.image("assets\surya.png", width=120)
+        st.image("assets/surya.png", width=120)
         st.markdown("**Suryashis Banerjee**  \nCSE Student")
 
     st.markdown("---")
@@ -56,7 +56,7 @@ def show_about_page():
     col1, col2 = st.columns(2)
 
     with col1:
-        st.image("assets\\award.png", caption="State Science Congress", use_container_width=True)
+        st.image("assets/award.png", caption="State Science Congress", use_container_width=True)
 
     with col2:
-        st.image("assets\certificate.png", caption="Project Recognition", use_container_width=True)
+        st.image("assets/certificate.png", caption="Project Recognition", use_container_width=True)
