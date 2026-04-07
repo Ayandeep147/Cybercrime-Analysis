269e9939b125084410fce7c3da6fcb30e9bba050
Ayandeep <ayandeeproy460@gmail.com>
2026-04-07 22:37:41 +0530

    fixed background path

--- main.py
+++ main.py
@@ -5,21 +5,25 @@ from src.ml_page import show_ml_page
 from src.utils import get_district_table
 from src.about import show_about_page
 import base64
+import os
+import streamlit as st
 
 def set_bg():
-    with open("assets\Gemini_Generated_Image_rr7zpqrr7zpqrr7z.pngs", "rb") as f:
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    image_path = os.path.join(BASE_DIR, "assets", "bg.png")
+
+    with open(image_path, "rb") as f:
         data = base64.b64encode(f.read()).decode()
 
     st.markdown(f"""
     <style>
     .stApp {{
-        background-image: url("data:image/jpg;base64,{data}");
+        background-image: url("data:image/png;base64,{data}");
         background-size: cover;
+        background-position: center;
     }}
     </style>
     """, unsafe_allow_html=True)
-
-set_bg()
 
 
 st.set_page_config(page_title="A Hybrid Machine Learning Framework for Geospatial Cyber Crime Prediction and Demographic Pattern Analysis", layout="wide")
