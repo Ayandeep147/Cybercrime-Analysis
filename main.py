2d059f3c877820987fa6bc837ec989b7f6ce52e8
Ayandeep Roy <ayandeeproy460@gmail.com>
2026-04-26 19:57:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from src.about import show_about_page
 import base64
 
 def set_bg():
-    with open("assets/abstract-techno-background-with-connecting-dots.jpg.jpeg", "rb") as f:
+    with open("assets/Abstract Technology.jpg.jpeg", "rb") as f:
         data = base64.b64encode(f.read()).decode()
 
     st.markdown(f"""
